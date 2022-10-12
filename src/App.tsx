c2c7e8b98d6a5900493fba3411eaa2dbfab41a49
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-10-12 14:14:30 -0300

    feat: 🎸 storybook component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ export function App() {
 
   return (
     <>
-    <h1>world</h1>
+    <h1 className='bg-cyan-500 text-xl font-bold'>world</h1>
+
+    <button className='bg-cyan-500 hover:bg-cyan-300'>Enviar</button>
     </>
   )
 }
