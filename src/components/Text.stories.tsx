6e25ca43044e14b6520bb34fb0500644688f2eb9
matheusht <dev.matheustheodoro@gmail.com>
2022-10-13 12:17:09 -0300

    feat: 🎸 Meta and StoryObj addition

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,6 +1,15 @@
-import { Text } from './Text';
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text';
 
 export default {
     title: 'Components/Text',
     component: Text,
+    args: {
+        children: 'Tchuben',
+        size: 'md',
+    },
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {
+
 }
